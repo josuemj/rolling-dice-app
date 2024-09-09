9a3b23ec3e56e3d75126261075f84d8115a27650
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-08 21:36:02 -0600

    feat: created main app container

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Container(
-        color: Colors.cyan,
-        child: Image.asset('assets/images/dice-1.png'),
-      ),
-    ),
-  );
+  runApp(MaterialApp());
 }
---
+++ lib/screens/main_container.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppContainer extends StatelessWidget {
+  const AppContainer({super.key, required this.gradientColors});
+
+  final List<Color> gradientColors;
+
+  @override
+  Widget build(context) {
+    return (Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: gradientColors),
+      ),
+    ));
+  }
+}
