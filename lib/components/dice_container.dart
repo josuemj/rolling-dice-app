74dc7c8d61e38d1704841102e33c881326e49678
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-09 23:21:14 -0600

    feat: Introducing states

--- lib/components/dice_container.dart
+++ lib/components/dice_container.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
 
-class DiceContainer extends StatelessWidget {
-  const DiceContainer({super.key});
-
-  void roll() {}
+class DiceContainerState extends StatefulWidget {
+  const DiceContainerState({super.key});
 
   @override
-  Widget build(context) {
+  State<DiceContainerState> createState() {
+    return _DiceContainerState();
+  }
+}
+
+class _DiceContainerState extends State<DiceContainerState> {
+  var currentDice = 1;
+
+  void rollDice() {
+    setState(() {
+      if (currentDice >= 6) {
+        currentDice = 1;
+      } else {
+        currentDice += 1;
+      }
+    });
+  }
+
+  @override
+  build(context) {
     return Center(
       child: (Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset(
-            'assets/images/dice-6.png',
-            width: 200,
+            'assets/images/dice-$currentDice.png',
+            width: 300,
           ),
           TextButton(
-            onPressed: roll,
+            onPressed: rollDice,
             style: TextButton.styleFrom(
-              padding: const EdgeInsets.only(top: 30),
               foregroundColor: Colors.white,
+              padding: const EdgeInsets.only(top: 20),
               textStyle: const TextStyle(
-                fontSize: 28,
+                fontSize: 26,
               ),
             ),
-            child: const Text("Roll"),
-          ),
+            child: const Text('Roll dice'),
+          )
         ],
       )),
     );
--- lib/screens/main_container.dart
+++ lib/screens/main_container.dart
@@ -16,7 +16,7 @@ class AppContainer extends StatelessWidget {
           begin: Alignment.bottomRight,
         ),
       ),
-      child: DiceContainer(),
+      child: const DiceContainerState(),
     ));
   }
 }
